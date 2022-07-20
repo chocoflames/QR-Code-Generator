a77c34c3abb9d20e3e467faa68acfd3ebd97d548
chocoflames <ezevalentine107@gmail.com>
2022-07-20 10:15:45 +0100

    QR Code project completed

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const wrapper = document.querySelector(".wrapper");
+const qrInput = document.querySelector(".form input");
 const generateBtn = document.querySelector(".form button");
+const qrImg = document.querySelector(".qr-code img");
 
 generateBtn.addEventListener("click", () => {
-    wrapper.classList.add("active")
+  let qrValue = qrInput.value;
+  if (!qrValue) return; //if the code is empty then return from here
+  generateBtn.innerHTML = "Generating QR Code...";
+  //getting a QR code of user entered value using the qr server
+  //api and pushing the api returned img src  to qrImg
+  qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrValue}`;
+  qrImg.addEventListener("load", () => { //once QR Code img loaded
+    wrapper.classList.add("active");
+    generateBtn.innerHTML = "Generate QR Code";
+  });
+});
+
+qrInput.addEventListener("keyup", () => {
+    if(!qrInput.value){
+        wrapper.classList.remove("active");
+    }
 })
