79020ee6b31f5410198b5878d9f22d84410a2fd9
chocoflames <ezevalentine107@gmail.com>
2022-07-20 09:08:48 +0100

    added images

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-const wrapper = document.querySelector
+const wrapper = document.querySelector(".wrapper");
+const generateBtn = document.querySelector(".form button");
+
+generateBtn.addEventListener("click", () => {
+    wrapper.classList.add("active")
+})
